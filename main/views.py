f500b7ebb52e4edee29f4640a8345d5ee7e51e5f
aizirek <aaizirek499@gmail.com>
2022-09-22 19:31:40 +0300

    copied index.html with static

--- main/views.py
+++ main/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import render
 def homepage (request):
     return render (request,'index.html')
 
-def chat (request):
-    return render (request,'names.html')
-
-def get (request):
-    return HttpResponse('I LOVE YOU')
 
 
 # Create your views here.
--- todo/urls.py
+++ todo/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage,chat,get
+from main.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage)
-    path ('chat/',chat)
-    path ('get/',get)
+    
 ]
