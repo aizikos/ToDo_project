d9e1d379bce873a521ce2fa039a23654346fc23b
aizirek <aaizirek499@gmail.com>
2022-09-24 22:33:04 +0300

    set media and static urls and roots

--- todo/settings.py
+++ todo/settings.py
@@ -116,15 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR /'static'
 
 
-MEDiA_URL = 'media'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- todo/urls.py
+++ todo/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',homepage)
+    path('',homepage),
     
 ]   + static(settings.STATIC_URL,document_root= settings.STATIC_ROOT) \
-    + static(settings.MEDiA_URL, document_root= settings.MEDIA_ROOT)
+    + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
