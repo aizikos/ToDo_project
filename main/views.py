c23401383d8887bc6e1b2fcd8af33a3bbc378944
aizirek <aaizirek499@gmail.com>
2022-09-24 23:36:14 +0300

    copied test.html with static

--- main/views.py
+++ main/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 def homepage (request):
     return render (request,'index.html')
 
+def test (request):
+    return render (request,'test.html')
+
+
+
 
 
 # Create your views here.
--- todo/urls.py
+++ todo/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage
+from main.views import homepage,test
+#from homework.views import test*
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage),
+    path("test/", test, name="test"),
     
 ]   + static(settings.STATIC_URL,document_root= settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
