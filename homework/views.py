7e33c482245b3a0768cf08c921657c5a4f37d1a6
aizirek <aaizirek499@gmail.com>
2022-10-01 01:34:33 +0300

    changed files homework/views.py,main/views.py,todo/urls.py and added homework/static/styles/meet.css

--- homework/views.py
+++ homework/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import ToMeet
+
 def work (request):
     return render (request,'index1.html')
+
+def works (request):
+    todo_list = ToMeet.objects.all()
+    return render (request,'meeting.html',{"todo_list" : todo_list})
+
 
 # Create your views here.
