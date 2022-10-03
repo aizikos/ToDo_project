5975ac8a066d4dbdaf85736323b4066fa20fed7e
aizirek <aaizirek499@gmail.com>
2022-10-03 17:29:49 +0300

    many changes in the file main

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class TODO(models.Model):
+    text = models.CharField(max_length = 100)
+    date = models.DateTimeField(auto_now_add= True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
+#from .models import ToDo
 
 def homepage (request):
-    return render (request,'index.html',)
+        return render (request,'index.html')
 
-def test (request):
-    return render (request,'test.html')
+def test (request):   
+    return render (request,'test.html',)
 
+#def add_todo(request):
+    #f = request.POST
+   # text=f["todo_text"]
+    #todo = ToDo(
+    #    text=text
+    
+    #todo.save()
+
+    #return HttpResponse("FORMA")
 
 
 
