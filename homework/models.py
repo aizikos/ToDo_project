dcbb84a7c776b59fcdbf3c454fcdd0ae5f1581e4
aizirek <aaizirek499@gmail.com>
2022-10-05 19:50:00 +0300

    added models ToMeet and Habits

--- homework/models.py
+++ homework/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class ToMeet(models.Model):
     persone = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
-    date_of_meeting = models.DateTimeField(auto_now_add = False)
-    comment = models.TextField()
+    phone_number = models.IntegerField(default= True)
+    date_of_meeting = models.DateTimeField(auto_now_add = True)
+    comment = models.TextField(default="")
     is_closed =models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class Habits(models.Model):
+    name = models.CharField(max_length = 100)
+    done_for_today = models.TextField(default="")
+    important = models.TextField(default="")
+    comment = models.TextField(default="")
 
 # Create your models here.
--- homework/views.py
+++ homework/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
 
-from .models import ToMeet
+from .models import ToMeet, Habits
 
 def work (request):
     return render (request,'index1.html')
@@ -8,6 +8,47 @@ def work (request):
 def works (request):
     todo_list = ToMeet.objects.all()
     return render (request,'meeting.html',{"todo_list" : todo_list})
+
+def meet (request):
+    todo_list = ToMeet.objects.all()
+    return render (request,'Toomet1.html',{"todo_list": todo_list})
+
+def add_todos(request):
+    f = request.POST
+    text = f["todo_text"]
+    num = f ["todo_number"]
+    com = f ["todo_com"]
+    todo = ToMeet(
+        persone = text,
+
+        phone_number=num,
+
+        comment = com
+    )
+    todo.save()
+    return redirect(meet)
+    
+def habits(request):
+    todo_list = Habits.objects.all()
+    return render (request,"habits.html",{"todo_list": todo_list})
+
+def add_habits(request):
+    f = request.POST
+    name = f["habits_text"]
+    num = f ["habits_day"]
+    hot=f ["habits_hot"]
+    com = f ["habits_com"]
+    hab = Habits(
+        name = name,
+
+        done_for_today=num,
+
+        important= hot,
+
+        comment = com
+    )
+    hab.save()
+    return redirect(habits)
 
 
 # Create your views here.
